e64897ac30ee3272f69e6248874e026de33292a0
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-03-27 15:51:38 -0300

    feat: add specifications Router

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import express from "express";
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes)
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
-
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => console.log("Server is running on port 3333!"));
