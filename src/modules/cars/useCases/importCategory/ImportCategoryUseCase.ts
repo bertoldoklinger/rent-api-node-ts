e8d489f910d90ce37fa33e7c47f64d01a17d52b5
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-03-29 16:31:06 -0300

    feat: add ImportCategoryUseCase methods and implementation

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,40 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
+interface IImportCategory {
+  name: string
+  description: string
+}
 
 class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) { }
 
-  execute(file: Express.Multer.File): void {
-    console.log(file)
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = []
+      const stream = fs.createReadStream(file.path);
+
+      const parseFile = parse()
+
+      stream.pipe(parseFile)
+
+      parseFile.on("data", async (line) => {
+        const [name, description] = line
+        categories.push({ name, description })
+      })
+        .on("end", () => {
+          resolve(categories)
+        })
+        .on("error", (err) => {
+          reject(err)
+        })
+    })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    console.log(categories)
   }
 }
 
